45680aa6866f12127b23db8e907fb6ab9f0c918f
sdlin <sdlin88@gmail.com>
2013-10-01 19:36:22 -0700

    added locations and a map to asciiart

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ import re
 import string
 import hashlib
 import random
+import urllib2
+from xml.dom import minidom
 
 from google.appengine.ext import db
 
@@ -161,16 +163,44 @@ class Rot13Handler(BaseHandler):
 		mytext = self.request.get("text")
 		self.write_page(self.Rot13(mytext))
 
+def get_coords(ip):
+	IPurl = 'http://api.hostip.info/?ip='
+	url = IPurl + ip
+	content = None
+	try:
+		content = urllib2.urlopen(url).read()
+	except:
+		return
+	if content:
+	    d = minidom.parseString(content)
+	    coords = d.getElementsByTagName('gml:coordinates')
+	    if coords:
+	        lon, lat =  coords[0].childNodes[0].nodeValue.split(',')
+	        return db.GeoPt(lat, lon)
+
+def gmaps_img(points):
+    GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x280&sensor=false&"
+    getparams = '&'.join('markers=%s,%s' % (p.lat,p.lon) for p in points)
+    return GMAPS_URL + getparams
+
 class Art(db.Model):
 	title = db.StringProperty(required = True)
 	arttext = db.TextProperty(required = True)
 	date_created = db.DateTimeProperty(auto_now_add = True)
+	coords = db.GeoPtProperty()
 
 class AsciiArtHandler(BaseHandler):
 	def render_front(self, title="", arttext="", error=""):
 		arts = db.GqlQuery("SELECT * FROM Art "
 						   "ORDER BY date_created DESC ")
-		self.render("asciiart.html", title=title, arttext=arttext, error=error, arts=arts)
+		arts = list(arts)
+		points = filter(None, (a.coords for a in arts))
+		img_url = None
+		if points:
+			img_url = gmaps_img(points)
+
+		self.render("asciiart.html", title=title, arttext=arttext, error=error, arts=arts, mapsurl=img_url)
+
 	
 	def get(self):
 		self.render_front()
@@ -181,6 +211,9 @@ class AsciiArtHandler(BaseHandler):
 
 		if title and arttext:
 			a = Art(title=title, arttext=arttext)
+			coords = get_coords(self.request.remote_addr)
+			if coords:
+				a.coords = coords
 			a.put()
 			self.redirect("/asciiart")
 		else:
